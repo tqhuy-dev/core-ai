b4bfa457159341fb23ad980d8788d6ef118c5277
huytq1 <huytq1@ghn.vn>
2025-05-22 17:53:56 +0700

    code

--- agents/rag_agents_lang_chain.py
+++ agents/rag_agents_lang_chain.py
@@ -3,6 +3,12 @@ from langchain.memory import ConversationBufferMemory
 from langchain_chroma import Chroma
 from langchain_core.callbacks import StdOutCallbackHandler
 from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+from langchain_core.tools import tool
+class SearchInternalDocuments(BaseModel):
+    """Search for internal documents"""
+    query: str = Field(..., description="The query to search for internal documents")
 
 
 class RAGAgentLangChainOpenAI(object):
@@ -25,3 +31,12 @@ class RAGAgentLangChainOpenAI(object):
 
     def invoke(self, input_text: str):
         return self.conversation_chain.invoke({"question": input_text})
+
+
+def rag_search_internal_documents(ragAgents: RAGAgentLangChainOpenAI):
+    @tool("search_internal_documents", args_schema=SearchInternalDocuments)
+    def search_internal_documents_tool(query: str) -> str:
+        result = ragAgents.invoke(query)
+        return result["answer"]
+
+    return search_internal_documents_tool
