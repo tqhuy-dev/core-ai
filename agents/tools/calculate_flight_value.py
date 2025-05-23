3491edff3fb20928275b3ca25dedec47e3967fd5
huytq1 <huytq1@ghn.vn>
2025-05-23 12:27:59 +0700

    code

--- agents/tools/calculate_flight_value.py
+++ agents/tools/calculate_flight_value.py
@@ -1,19 +1,24 @@
 from pydantic import BaseModel, Field
 from langchain_core.tools import tool
 class CalculateFlightValue(BaseModel):
-    """Calculate the value of a flight"""
+    """Calculate the value of a flight based on location and brand"""
     flight_location: str = Field(..., description="The location of the flight to calculate the value for")
+    flight_brand: str = Field(..., description="The brand of the flight to calculate the value for")
 
 @tool("flight_value_tool" , args_schema=CalculateFlightValue)
-def calculate_flight_value(flight_location: str) -> int:
+def calculate_flight_value(flight_location: str,flight_brand: str) -> int:
+    print(f"flight_location: {flight_location} flight_brand: {flight_brand}")
+    bonus_fee = 0
+    if flight_brand == "France":
+        bonus_fee = 1000
     if flight_location == "London":
-        return 1000
+        return 1000 + bonus_fee
     elif flight_location == "Paris":
-        return 2000
+        return 2000 + bonus_fee
     elif flight_location == "Berlin":
-        return 3000
+        return 3000 + bonus_fee
     else:
-        return 500
+        return 500 + bonus_fee
 
 
 
