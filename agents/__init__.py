50498081b9d23ee9efe773357db6e889a9044408
huytq1 <huytq1@ghn.vn>
2025-05-23 10:59:40 +0700

    code

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,4 +1,4 @@
-from .rag_agents_lang_chain import RAGAgentLangChainOpenAI
+from .rag_agents_lang_chain import RAGAgentLangChainOpenAI,rag_search_internal_documents
 from .agents_with_tools import AgentsOpenAIWithTools
 from .tools import *
 from .dto import *
